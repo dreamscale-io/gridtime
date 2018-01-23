096135e7e22096cf978378f3a28d3347716c8fde
Mike Lueders <mike@roadkill.org>
2018-01-22 21:05:21 -0600

    added swagger support

--- src/main/java/org/dreamscale/htmflow/HtmFlow.java
+++ src/main/java/org/dreamscale/htmflow/HtmFlow.java
@@ -3,8 +3,10 @@ package org.dreamscale.htmflow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(SwaggerConfig.class)
 @ComponentScan("org.dreamscale.htmflow")
 public class HtmFlow {
 
