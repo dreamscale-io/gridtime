048b10516d96854f78319ab9e6649b46b36e201e
Janelle Klein <janelle@openmastery.org>
2018-01-23 16:39:46 -0600

    Add project data bootstrapping logic

---
+++ src/main/java/com/dreamscale/htmflow/BootstrapInitializer.java
@@ -0,0 +1,28 @@
+package com.dreamscale.htmflow;
+
+import com.dreamscale.htmflow.core.context.domain.ProjectEntity;
+import com.dreamscale.htmflow.core.context.domain.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.UUID;
+
+@Component
+public class BootstrapInitializer {
+
+    @Autowired
+    private ProjectRepository repository;
+
+    @PostConstruct
+    private void bootstrap() {
+        repository.save(
+                ProjectEntity.builder()
+                .id(UUID.randomUUID())
+                .name("Test Project")
+                .externalId("12345")
+                .build()
+        );
+    }
+
+}
