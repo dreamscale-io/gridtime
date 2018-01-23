235fd52c7224f3d9c3972fd854391182d6655ab7
Janelle Klein <janelle@openmastery.org>
2018-01-23 16:43:20 -0600

    Move Swagger package to com

--- src/main/java/com/dreamscale/htmflow/HtmFlow.java
+++ src/main/java/com/dreamscale/htmflow/HtmFlow.java
@@ -1,6 +1,5 @@
 package com.dreamscale.htmflow;
 
-import org.dreamscale.htmflow.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
--- src/main/java/com/dreamscale/htmflow/SwaggerConfig.java
+++ src/main/java/com/dreamscale/htmflow/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package org.dreamscale.htmflow;
+package com.dreamscale.htmflow;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
