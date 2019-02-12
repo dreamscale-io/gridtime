7464065b506ebabafa1326a2201fb6875bfcd86f
Janelle Klein <janelle@dreamscale.io>
2019-02-12 10:29:18 -0600

    FP-167: Add paging to activity updates to avoid running out of memory

--- src/main/java/com/dreamscale/htmflow/core/job/UpdateFlowActivityJob.java
+++ src/main/java/com/dreamscale/htmflow/core/job/UpdateFlowActivityJob.java
@@ -4,6 +4,9 @@ import com.dreamscale.htmflow.core.domain.flow.FlowActivityEntity;
 import com.dreamscale.htmflow.core.domain.flow.FlowActivityRepository;
 import com.dreamscale.htmflow.core.service.ComponentLookupService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -16,17 +19,28 @@ public class UpdateFlowActivityJob {
     ComponentLookupService componentLookupService;
 
     public void run() {
-        Iterable<FlowActivityEntity> allActivity = flowActivityRepository.findAll();
 
-        for (FlowActivityEntity flowActivityEntity : allActivity) {
+        PageRequest firstPageRequest = new PageRequest(0, 1000);
+        Page<FlowActivityEntity> firstPage = flowActivityRepository.findAll(firstPageRequest);
 
-            String filePath = flowActivityEntity.getMetadataValue("filePath");
-            String component = componentLookupService.lookupDefaultComponent(filePath);
+        int totalPages = firstPage.getTotalPages();
 
-            if (flowActivityEntity.getComponent() == null) {
-                flowActivityEntity.setComponent(component);
-                flowActivityRepository.save(flowActivityEntity);
+        for (int pageIndex = 0; pageIndex < totalPages; pageIndex++) {
+            PageRequest pageRequest = new PageRequest(pageIndex, 1000);
+
+            Page<FlowActivityEntity> currentPage = flowActivityRepository.findAll(pageRequest);
+
+            for (FlowActivityEntity flowActivityEntity : currentPage) {
+
+                String filePath = flowActivityEntity.getMetadataValue("filePath");
+                String component = componentLookupService.lookupDefaultComponent(filePath);
+
+                if (flowActivityEntity.getComponent() == null) {
+                    flowActivityEntity.setComponent(component);
+                    flowActivityRepository.save(flowActivityEntity);
+                }
             }
         }
+
     }
 }
