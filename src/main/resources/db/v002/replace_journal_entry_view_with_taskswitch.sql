336813d587802e37bcc6bea70d2fea717d9a3905
Janelle Klein <janelle@dreamscale.io>
2020-01-11 11:10:46 -0600

    Revert accidentally changed files.

--- src/main/resources/db/v002/replace_journal_entry_view_with_taskswitch.sql
+++ src/main/resources/db/v002/replace_journal_entry_view_with_taskswitch.sql
@@ -13,14 +13,14 @@ create view journal_entry_view as
          join project p on i.project_id = p.id
   UNION ALL
   select c.id, c.position, c.description, c.project_id, c.task_id, c.organization_id, c.member_id,
-         c.flame_rating, c.finish_status, c.finish_time, c.circle_id, 'WTF_ROOM' as journal_entry_type,
+         c.flame_rating, c.finish_status, c.finish_time, c.circle_id, 'WTF' as journal_entry_type,
          t.name task_name, t.summary task_summary, p.name project_name
   from circle_context c
          join task t on c.task_id = t.id
          join project p on c.project_id = p.id
   UNION ALL
   select ts.id, ts.position, ts.description, ts.project_id, ts.task_id, ts.organization_id, ts.member_id,
-        null as flame_rating, null as finish_status, null as finish_time, null as circle_id, 'TaskSwitch' as journal_entry_type,
+         null as flame_rating, null as finish_status, null as finish_time, null as circle_id, 'TaskSwitch' as journal_entry_type,
          t.name task_name, t.summary task_summary, p.name project_name
   from task_switch_event ts
          join task t on ts.task_id = t.id
