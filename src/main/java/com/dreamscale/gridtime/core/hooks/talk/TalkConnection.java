e0f19ce0dba655c86b10ffe10113b6002173352f
Arty Starr <arty@dreamscale.io>
2020-04-23 08:07:35 -0500

    FP-400: Add error handling for talk message failures

--- src/main/java/com/dreamscale/gridtime/core/hooks/talk/TalkConnection.java
+++ src/main/java/com/dreamscale/gridtime/core/hooks/talk/TalkConnection.java
@@ -3,14 +3,17 @@ package com.dreamscale.gridtime.core.hooks.talk;
 import com.dreamscale.gridtime.api.account.SimpleStatusDto;
 import com.dreamscale.gridtime.api.circuit.ChatMessageDetailsDto;
 import com.dreamscale.gridtime.api.circuit.TalkMessageDto;
+import com.dreamscale.gridtime.api.status.Status;
 import com.dreamscale.gridtime.core.hooks.talk.dto.ClientConnectionDto;
 import com.dreamscale.gridtime.core.hooks.talk.dto.CircuitMessageType;
 import com.dreamscale.gridtime.core.machine.commons.JSONTransformer;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.UUID;
 
+@Slf4j
 public class TalkConnection {
 
     private final TalkClient talkClient;
@@ -20,19 +23,56 @@ public class TalkConnection {
     }
 
     public SimpleStatusDto joinRoom(UUID connectionId, UUID roomId) {
-        return talkClient.joinRoom(roomId.toString(), new ClientConnectionDto(connectionId));
+        SimpleStatusDto status = null;
+
+        try {
+            status = talkClient.joinRoom(roomId.toString(), new ClientConnectionDto(connectionId));
+        } catch (Exception ex) {
+            log.error("TalkConnection.joinRoom", ex);
+            status = new SimpleStatusDto(Status.FAILED, "TalkConnection.joinRoom "+ex.getMessage());
+        }
+
+        return status;
     }
 
+
     public SimpleStatusDto leaveRoom(UUID connectionId, UUID roomId) {
-        return talkClient.leaveRoom(roomId.toString(), new ClientConnectionDto(connectionId));
+        SimpleStatusDto status = null;
+
+        try {
+            status = talkClient.leaveRoom(roomId.toString(), new ClientConnectionDto(connectionId));
+        } catch (Exception ex) {
+            log.error("TalkConnection.leaveRoom failed", ex);
+            status = new SimpleStatusDto(Status.FAILED, "TalkConnection.leaveRoom: "+ex.getMessage());
+        }
+
+        return status;
     }
 
     public SimpleStatusDto sendDirectMessage(UUID toConnectionId, TalkMessageDto talkMessageDto) {
-        return talkClient.sendDirectMessage(toConnectionId.toString(), talkMessageDto);
+        SimpleStatusDto status = null;
+
+        try {
+            status = talkClient.sendDirectMessage(toConnectionId.toString(), talkMessageDto);
+        } catch (Exception ex) {
+            log.error("TalkConnection.sendDirectMessage failed", ex);
+            status = new SimpleStatusDto(Status.FAILED, "TalkConnection.sendDirectMessage failed: "+ex.getMessage());
+        }
+
+        return status;
     }
 
     public SimpleStatusDto sendRoomMessage(UUID roomId, TalkMessageDto talkMessageDto) {
-        return talkClient.sendRoomMessage(roomId.toString(), talkMessageDto);
+        SimpleStatusDto status = null;
+
+        try {
+            status = talkClient.sendRoomMessage(roomId.toString(), talkMessageDto);
+        } catch (Exception ex) {
+            log.error("TalkConnection.sendRoomMessage failed", ex);
+            status = new SimpleStatusDto(Status.FAILED, "TalkConnection.sendRoomMessage failed: "+ex.getMessage());
+        }
+
+        return status;
     }
 
 
