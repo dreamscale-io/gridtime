7054d1facb8584c6c09426c9ba8f33ee90cc4ed8
Mike Lueders <mike@roadkill.org>
2020-05-29 17:59:53 -0500

    fix jackson

--- src/main/java/com/dreamscale/gridtime/GridTime.java
+++ src/main/java/com/dreamscale/gridtime/GridTime.java
@@ -2,14 +2,23 @@ package com.dreamscale.gridtime;
 
 import com.dreamscale.gridtime.core.security.RequestContext;
 import com.dreamscale.gridtime.core.security.WebSecurityConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.dreamscale.feign.DefaultFeignConfig;
 import org.dreamscale.springboot.config.CommonSpringBootConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
 
 @SpringBootApplication
 @ComponentScan("com.dreamscale.gridtime")
@@ -25,6 +34,34 @@ public class GridTime {
     public static void main(String[] args) {
 
         SpringApplication.run(GridTime.class, args);
+    }
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    /**
+     * Even though the ObjectMapper is defined as @Primary in CommonSpringConfig, it is not getting used to convert
+     * the request/responses.  There is a potential explanation of why here https://springfox.github.io/springfox/docs/current/,
+     * under the section "Object Mapper Customizations Not Working?".  While the provided solution in those docs does
+     * not seem work, specifically injecting the ObjectMapper by name and setting it directly on the
+     * MappingJackson2HttpMessageConverter does.
+     */
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+                for (HttpMessageConverter converter : converters) {
+                    if (converter instanceof MappingJackson2HttpMessageConverter) {
+                        MappingJackson2HttpMessageConverter jacksonConverter =
+                                ((MappingJackson2HttpMessageConverter) converter);
+                        if (jacksonConverter.getObjectMapper() != null) {
+                            jacksonConverter.setObjectMapper(objectMapper);
+                        }
+                    }
+                }
+            }
+        };
     }
 
     @Bean
