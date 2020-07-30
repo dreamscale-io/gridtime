2074b543a0c7703eb41c845ba424edceb45c4398
Arty Starr <arty@dreamscale.io>
2020-07-30 13:19:19 -0500

    Fix memory leak issue with object mapper instance creation

--- src/main/java/com/dreamscale/gridtime/core/mapper/MapperFactory.java
+++ src/main/java/com/dreamscale/gridtime/core/mapper/MapperFactory.java
@@ -4,6 +4,7 @@ import org.dozer.DozerBeanMapper;
 import org.dozer.Mapper;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Collections;
 import java.util.List;
 
@@ -12,15 +13,18 @@ public class MapperFactory {
 
     private List<String> mappingConfigFileList = Collections.singletonList("dozerConfig.xml");
 
-    public Mapper createMapper() {
+    private DozerBeanMapper sharedMapper;
+
+
+    @PostConstruct
+    void initMapper() {
         DozerBeanMapper mapper = new DozerBeanMapper();
         mapper.setMappingFiles(mappingConfigFileList);
-        return mapper;
+        this.sharedMapper = mapper;
     }
 
     public <A, E> DtoEntityMapper<A, E> createDtoEntityMapper(Class<A> dtoType, Class<E> entityType) {
-        Mapper mapper = createMapper();
-        return new DtoEntityMapper<>(mapper, dtoType, entityType);
+        return new DtoEntityMapper<>(sharedMapper, dtoType, entityType);
     }
 
 }
